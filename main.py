e867e07b6263a5eabfc256f392221481c9e198da
coxxy <ezeiyokecosmas@gmail.com>
2022-11-27 18:48:17 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+for n in range (0, 16) :
+     
+  if n % 3 == 0 and n % 5 == 0 :
+    print('FIZZBUZZ')
+        
+  elif n % 5 == 0 :
+   print('BUZZ')
+
+  elif  n % 3 == 0 :
+   print('FIZZ')
+
+  else:
+    print('NUMBER')
+    
+
+
